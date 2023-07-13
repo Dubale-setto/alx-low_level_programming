543d96abec0b17824d1008f7acbdacb7cf156890
Dubale-setto <dubalesetto10@gmail.com>
2023-07-13 05:34:44 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,13 @@
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,|n);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of short int: %ld byte(s)\n", sizeof(short int));
+	printf("Size of int: %ld byte(s)\n", sizeof(int));
+	printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of float: %ld byte(s)\n", sizeof(float));
+	printf("Size of double: %ld byte(s)\n", sizeof(double));
+	printf("Size of long double: %ld byte(s)\n", sizeof(long double));	
 	return (0);
 }
+
