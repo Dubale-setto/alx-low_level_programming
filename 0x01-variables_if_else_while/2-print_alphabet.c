8656e5722bcb7c7b66440fb2552c9072fce171a3
Dubale-setto <dubalesetto10@gmail.com>
2023-07-14 07:54:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the last digit of a random number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+while (n <= 122)
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+return (0);
+}
