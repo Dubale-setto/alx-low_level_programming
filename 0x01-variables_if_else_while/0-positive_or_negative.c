0ebd5206c62e0cd227944b33dcb96b762628aafe
Dubale-setto <dubalesetto10@gmail.com>
2023-07-14 06:57:08 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-
+/**
+ * main - Entry point
+ *
+ * Description: This program prints "Hello, Holberton" to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
-	n = rand () - RAND_MAX / 2;
+
+	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
 	}
-else if (n == 0)
+	else if (n == 0)
 	{
 	printf("%d is zero\n", n);
 	}
-else
+	else
 	{
 	printf("%d is negative\n", n);
 	}
