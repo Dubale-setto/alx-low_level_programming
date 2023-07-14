8c4b7f25146a79d9040f8e58b83cad7bd195b141
Dubale-setto <dubalesetto10@gmail.com>
2023-07-14 07:15:28 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the last digit of a random number.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	har c;
+for (c = 'a'; c <= 'z'; ++c)
+	putchar(c);
+for (c = 'A'; c <= 'Z'; ++c)
+	putchar(c);
+	putchar('\n');
+return (0);
+}
