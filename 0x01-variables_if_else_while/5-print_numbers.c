facb8f4e14b01ab7fb6d8c826325e5d7fbf86c83
Dubale-setto <dubalesetto10@gmail.com>
2023-07-14 21:07:11 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the last digit of a random number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	
+	return(0);
+}
