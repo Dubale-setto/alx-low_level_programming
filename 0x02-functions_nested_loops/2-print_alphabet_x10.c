3d945c8fb823ff406a6d0401588df9a58f614605
Dubale-setto <dubalesetto10@gmail.com>
2023-07-18 03:39:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *void print_alphabet_x10 - prints the alphabet 10 times
+  *
+  *Return always 0 sucess
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97, j <= 122, j++)
+		{
+			_putchar(j);
+		}
+		_putchar(i);
+	}
+_putchan('\n');
+}
