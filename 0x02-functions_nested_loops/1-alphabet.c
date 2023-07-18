bb19e7328b979f2ef67ad7fc40f03e3f6695bdac
Dubale-setto <dubalesetto10@gmail.com>
2023-07-18 03:24:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
-   * main - Entry point
-   *
-    * Description: This function prints _putchar to the standard output.
+ * main - Entry point
  *
-   * Return: Always 0 (Success)
-   */
+ * Description: This function prints _putchar to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i <= 122; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
