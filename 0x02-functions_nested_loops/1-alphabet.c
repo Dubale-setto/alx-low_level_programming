44f30bfc0aa24064d21a4c7d08b739b5517208bb
Dubale-setto <dubalesetto10@gmail.com>
2023-07-18 03:22:20 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,16 +6,13 @@
  *
    * Return: Always 0 (Success)
    */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	int i;
+        int i;
 
-	for (i = 97; i <= 122; i++)
-
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-
-
+        for (i = 97; i <= 122; i++)
+        {
+                _putchar(i);
+        }
+        _putchar('\n');
 }
