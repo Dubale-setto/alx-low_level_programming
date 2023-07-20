5d77bce2746d9d8781907e5bc8ff5d285abd84cf
Dubale-setto <dubalesetto10@gmail.com>
2023-07-20 05:30:50 -0700

    l

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+  * main - Entry poin
+  ** Description: This program prints "Hello, Holberton" to the standard output.
+  *Return: Always 0 (Success
+  */
