69e6cd5c41e68287ad1119772dbd3a23ed977497
Dubale-setto <dubalesetto10@gmail.com>
2023-07-20 21:44:08 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+  *mul - product of 2 int
+  *@a: intiger 1
+*@b: intiger
+*
+  *
+  *Return: returns multiplre
+  */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
