876d08bbb11799432fa1c3a0809e040a206d6f9a
Dubale-setto <dubalesetto10@gmail.com>
2023-07-20 21:54:03 -0700

    k

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+include"main.h"
+/**
+*print_numbers - prints 0-9
+*
+*
+*Return; 0 always sucess
+*/
+void print_numbers(void)
+{
+	int n;
+for (n = 48; n <= 57; n++)
+	{
+	_putchar(n);
+	}
+	_putchar('\n');
+}
