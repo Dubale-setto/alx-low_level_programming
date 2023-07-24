69eb84d02e6362a729c0e1998a522336c803f2d5
Dubale-setto <dubalesetto10@gmail.com>
2023-07-24 04:38:37 -0700

    l

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints every other character of a string
+ * @a: string to be printed
+ *@b: string to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++) {
+		printf("%d", a[i]);
+	if (i != n - 1)
+		printf(", ");
+		}
+	printf("\n");
+}
+
