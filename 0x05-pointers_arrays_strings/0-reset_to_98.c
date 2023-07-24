a507b2b3dac977fb606037ab3338c477b0683c14
Dubale-setto <dubalesetto10@gmail.com>
2023-07-24 02:43:24 -0700

    l

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  */
+*swap_int -  sawpa wew
+*
+*Return: value
+*@a:input
+*@b: input
+*/
 
-void swap_int(int *a, int *b) 
+void swap_int(int *a, int *b)
 {
 	int temp = *a;
 *a = *b;
