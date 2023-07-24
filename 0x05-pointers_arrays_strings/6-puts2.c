3e954792a88f2acf1d54a74e844be245812f3bb7
Dubale-setto <dubalesetto10@gmail.com>
2023-07-24 06:13:46 -0700

    l

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,15 +11,15 @@ void puts2(char *str)
 	int i;
 	int j = 0;
 
-
-	while (str[j] != '\n' )
+	while (str[j] != '\0')
 	{
-	j++;
-	
-	}	
-	for (i = 0; i < j; i++)
-	_putchar(str[i]);
+        j++;
 	}
+
+	for (i = 0; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
-
