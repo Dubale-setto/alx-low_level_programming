d0ecd665fccf8caccdbcf61a88cbc96ba60321e8
Dubale-setto <dubalesetto10@gmail.com>
2023-07-31 04:47:43 -0700

    ./8-print_diagsums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void print_diagsums(int *a, int size);
