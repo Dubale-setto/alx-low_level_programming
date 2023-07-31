9273638efb38d38fba6d16440e07d20684bc2861
Dubale-setto <dubalesetto10@gmail.com>
2023-07-31 04:31:59 -0700

    6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-char *_memcpy(char *dest, char *src, unsigned int n);
+int _putchar(char c);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
